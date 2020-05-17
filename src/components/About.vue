98e0732ae6038fb2d9354aa8aacaa0b43402b4b8
Shawn <devcorehd@gmail.com>
2020-05-18 01:40:29 +0800

    adding files

---
+++ src/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="about">
+        <h1>this is {{their_name}}, about page </h1>
+        <p> hello shawn </p>
+    </div> 
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data(){
+    return{
+   
+     their_name: this.$route.params.name 
+      
+  }
+  }
+}
+</script>
